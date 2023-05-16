76c55dec81d3cf2571b70a963748b67599072fcc
MegMathis <mcdonough.megan25@gmail.com>
2023-05-16 15:09:12 -0400

    start of footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes></Routes>
+      <Footer />
     </>
   );
 }
