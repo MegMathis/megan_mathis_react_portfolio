e1c1dab792809f1f50a5dba12cfdda3b77462518
MegMathis <mcdonough.megan25@gmail.com>
2023-05-16 22:49:18 -0400

    deployment stuff in package json

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
-// import AboutMe from "./components/AboutMe";
-// import Portfolio from "./components/Portfolio";
-// import Resume from "./components/Resume";
-// import Contact from "./components/Contact";
-// import { Routes, Route } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <>
-//       <Header />
-//       <Routes>
-//         <Route path="/" element={<AboutMe />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/resume" element={<Resume />} />
-//         <Route path="/contact" element={<Contact />} />
-//       </Routes>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
-
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -33,19 +8,6 @@ import Contact from "./pages/Contact";
 
 function App() {
   const [page, setPage] = useState("AboutMe");
-
-  // const showPage = () => {
-  //   switch (page) {
-  //     case "AboutMe":
-  //       return <AboutMe />;
-  //     case "Portfolio":
-  //       return <Portfolio />;
-  //     case "Resume":
-  //       return <Resume />;
-  //     case "Contact":
-  //       return <Contact />;
-  //   }
-  // };
 
   const showPage = () => {
     if (page === "AboutMe") {
