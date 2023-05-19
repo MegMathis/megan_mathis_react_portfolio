4ce4c2c544c9f9ed0016117b96a260768f4c6707
MegMathis <mcdonough.megan25@gmail.com>
2023-05-18 22:57:25 -0400

    updated with project 3

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import currencyConverter from "../assets/currency_converter.png";
 import employeeTracker from "../assets/employee_tracker.png";
-import noteTaker from "../assets/note_taker.png";
+import devDirect from "../assets/devdirect.png";
 import petRoastingApp from "../assets/pet_roasting_app.png";
 import techBlog from "../assets/tech_blog.png";
 import weatherDashboard from "../assets/weather_dashboard.png";
@@ -54,68 +54,6 @@ const Portfolio = () => {
           </div>
 
           <div
-            style={{ backgroundImage: `url(${employeeTracker})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
-          >
-            {/* hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                Employee Tracker using MySQL and Inquirer
-              </span>
-              <div className="pt-8 text-center">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  <a
-                    href="https://drive.google.com/file/d/1-lpkoF1Ae0VxrKOpBGKiJZHI62Qj54Ez/view"
-                    target="_blank"
-                  >
-                    Application
-                  </a>
-                </button>
-
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  <a
-                    href="https://github.com/MegMathis/Employee_Tracker"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${noteTaker})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
-          >
-            {/* hover effect */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-black tracking-wider">
-                Note Taking App using Express.js and Deployment to Heroku
-              </span>
-              <div className="pt-8 text-center">
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  <a
-                    href="https://note-taker-by-meg.herokuapp.com/notes"
-                    target="_blank"
-                  >
-                    Application
-                  </a>
-                </button>
-
-                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                  <a
-                    href="https://github.com/MegMathis/Note_Taker"
-                    target="_blank"
-                  >
-                    GitHub
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div
             style={{ backgroundImage: `url(${petRoastingApp})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
@@ -138,6 +76,66 @@ const Portfolio = () => {
                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                   <a
                     href="https://github.com/MegMathis/Pet_Roasting_App"
+                    target="_blank"
+                  >
+                    GitHub
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${devDirect})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                devDirect- Group Project 3 using React, TailWindCSS, MongoDB,
+                Express.js, Node.js and Deployment to Heroku
+              </span>
+              <div className="pt-8 text-center">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <a href="https://dev-direct.herokuapp.com/" target="_blank">
+                    Application
+                  </a>
+                </button>
+
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <a
+                    href="https://github.com/MegMathis/DevDirect"
+                    target="_blank"
+                  >
+                    GitHub
+                  </a>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${employeeTracker})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+          >
+            {/* hover effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-black tracking-wider">
+                Employee Tracker using MySQL and Inquirer
+              </span>
+              <div className="pt-8 text-center">
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <a
+                    href="https://drive.google.com/file/d/1-lpkoF1Ae0VxrKOpBGKiJZHI62Qj54Ez/view"
+                    target="_blank"
+                  >
+                    Application
+                  </a>
+                </button>
+
+                <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  <a
+                    href="https://github.com/MegMathis/Employee_Tracker"
                     target="_blank"
                   >
                     GitHub
